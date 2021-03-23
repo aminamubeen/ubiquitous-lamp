f7028939e931934cf4fa3aff7fb81e6816a2b67b
Amina Bhatkar <amina.mubeen98@gmail.com>
2021-03-23 17:45:41 +0530

    New lines added by dev1

--- new-project/src/Hello.java
+++ new-project/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public void method1() {
 		System.out.println("Hello from method1");
 		System.out.println("Line 2");
-		System.out.println("Line added by developer2");
+		System.out.println("Line 3");
+		System.out.println("Line 4");
 	}
 }
